fce76afe866561d3d9fba1a8517946151d0ed3f6
Kseniya Khafizovanagornykh <kseniya.khafizovanagornykh@skola.taby.se>
2025-10-23 13:18:33 +0000

    Del 3: Loop med flera försök

--- main.py
+++ main.py
@@ -3,5 +3,21 @@ print(f"Hej {namn} och välkommen till spelet!")
 
 import random as rand
 hemligt_tal= rand.randint(1,10)
-gissning = input("Gissa ett tal mellan 1 och 10:")
-print(f"Tyvärr, det var {hemligt_tal}")
+gissning = int(input("Gissa ett tal mellan 1 och 10:"))
+if gissning == hemligt_tal:
+    print("Du har rätt!")
+else:
+    print(f"Tyvärr, det var {hemligt_tal}")
+print("Nu ska du köra igen, med flera försök!")
+
+antal_gissningar = 0
+while True:
+    gissning_igen = int(input("Ge mig gissning mellan 1 och 10:"))
+    antal_gissningar += 1
+    if gissning > hemligt_tal:
+        print(f"Försök {antal_gissningar}: Fel")
+    elif gissning < hemligt_tal:
+        print(f"Försök {antal_gissningar}: Fel")
+    else:
+        print(f"Försök {antal_gissningar}: Rätt gissat!")
+        break
