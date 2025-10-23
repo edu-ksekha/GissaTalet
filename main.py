59925a1597aa05944460b92a89afa24d94cb0fc5
Kseniya Khafizovanagornykh <kseniya.khafizovanagornykh@skola.taby.se>
2025-10-23 12:50:11 +0000

    Del 2: Slumpat tal och gissning

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 namn = input("Vad heter du?:")
 print(f"Hej {namn} och välkommen till spelet!")
+
+import random as rand
+hemligt_tal= rand.randint(1,10)
+gissning = input("Gissa ett tal mellan 1 och 10:")
+print(f"Tyvärr, det var {hemligt_tal}")
